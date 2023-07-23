34d3373d564e01ec44fa3ec977daaa858c2e7c95
FrancisCalizo <francisacalizo@gmail.com>
2023-07-22 18:31:31 -0600

    feat: inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
+
 import './App.css';
 
+const URL = 'https://capsinterviews.azurewebsites.net/api/composites?code=pn489yjzOHYDc'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		// TODO: Replace with Caps URL
+		fetch('data/composites.json')
+			.then(res => res.json())
+			.then(data => console.log(data))
+			.catch(err => console.log(err))
+	}, [])
+
+    return (
+		<div className="App">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
 
 export default App;
